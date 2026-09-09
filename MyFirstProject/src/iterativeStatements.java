052038f9977bd8650e5d903613f1994cf8e5ef2a
bajilogba <76887644+adamajilo@users.noreply.github.com>
2026-09-09 17:58:11 -0500

    Create iterativeStatements.java

---
+++ MyFirstProject/src/iterativeStatements.java
@@ -0,0 +1,59 @@
+Int y=0;
+ while (y>=0) {
+Y ++; // y= y+1
+System.out.println (y);
+}
+
+ int I=0;
+ while (i <= 5)
+     System.out.println (“I = ”+(I += 1))
+
+//While loop can be written without curved brackets
+//6 iterations
+
+ int j=0;
+ while (j++ <= 5)
+	System.out.print(j+ “ ”); 
+
+//Compares j value with 5 then increase 
+
+ int k=0
+ while (++k <= 5)
+	System.out.print(k+” “)
+
+
+//There is an alternative version of the whole statement and we call it a do-while loop, it has the following format.
+
+ do{
+Statement 2
+Statement 3
+} while (Boolean expression 1)
+
+ int z = 0
+ do {
+ z -= 1;
+System.out.println 
+
+}while (z>0)
+
+//Find and print out the smallest integer whose factorial is greater than 1,000,000.
+	Int n = 1, fac = 1;
+	
+	 while (fac <= 1,000,000){
+	//while n! Is not greater than 1 mil
+	 n+=1;
+	Fac = fac * n;
+	} //fac *=n;
+	System.out.println( ”The smallest integer whose factorial greater than 1,000,000 is ”+n)
+	
+  int n, l;
+ n= 25;
+ n= 30;
+
+ int p = 1
+String s = “”;
+ while (p != n && s.length <= 1) {
+ s = s + p;
+ p += 1; 
+System.out.print(”s= “+ s)
+} //p == n || s.length > l
