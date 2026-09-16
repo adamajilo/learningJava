ba8032372838625c28f823ee54a2610a7c9df8dc
bajilogba <76887644+adamajilo@users.noreply.github.com>
2026-09-16 17:55:51 -0500

    Update playwithfunctions.java

--- Projects/src/playwithfunctions.java
+++ Projects/src/playwithfunctions.java
@@ -23,5 +23,22 @@ System.out.println("Happy Birthday there you!");
 System.out.println("You are x years old!");
 System.out.println("Happy Birthday to you!");
 
+
+public class Main {
+  static void myMethod(String fname, int age) {
+    System.out.println(fname + " is " + age);
+  }
+
+  public static void main(String[] args) {
+    myMethod("Liam", 5);
+    myMethod("Jenny", 8);
+    myMethod("Anja", 31);
+  }
+}
+
+// Liam is 5
+// Jenny is 8
+// Anja is 31
+
     }
 }
