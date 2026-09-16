fbcd4ab91db9ca57ac2d477dceeed7b5ad039736
bajilogba <76887644+adamajilo@users.noreply.github.com>
2026-09-16 17:26:47 -0500

    Update playwithfunctions.java

--- Projects/src/playwithfunctions.java
+++ Projects/src/playwithfunctions.java
@@ -1,6 +1,7 @@
 
 
 	public class playwith functions {
+        public static
 for (int a = 0; a< 5; a++) {
 	int a = 0;
 	a++;
@@ -11,7 +12,18 @@ for (int b = 0; b<5; b++) {
 
 }
 int b = 0;
-//
+// can be declared 
+// same variable used in different blocks can be the same
+
+static void message() {
+
+System.out.println("Print out this message.");
+}
+public static void main(String[] args) {
+    message();
+}
+
+
 
 
 }
